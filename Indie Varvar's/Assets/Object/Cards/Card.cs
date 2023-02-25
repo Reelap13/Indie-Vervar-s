f1ded2a4d4930e743478ca95a7f6fc0449300d36
Reelap13 <116920397+Reelap13@users.noreply.github.com>
2023-02-25 19:55:03 +0300

    Enum of state card
    Add enum of state card and several method for abstract card class.

--- Indie Varvar's/Assets/Object/Cards/Card.cs
+++ Indie Varvar's/Assets/Object/Cards/Card.cs
@@ -4,5 +4,52 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] protected int mana;
+    [SerializeField] protected int _mana;
+
+    private StateCard _state;
+
+
+    private void ChangeState(StateCard state)
+    {
+        _state = state;
+        switch (_state)
+        {
+            case StateCard.ACTIVE:
+
+                break;
+            case StateCard.PASIVE:
+
+                break;
+            case StateCard.IN_DECK:
+
+                break;
+            case StateCard.NOT_PLAYABLE:
+
+                break;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (_state == StateCard.ACTIVE)
+        {
+            
+        }
+    }
+    private void OnMouseEnter()
+    {
+        if (_state == StateCard.PASIVE)
+        {
+            ChangeState(StateCard.ACTIVE);
+        }
+    }
+
+}
+
+enum StateCard
+{
+    ACTIVE,
+    PASIVE,
+    IN_DECK,
+    NOT_PLAYABLE
 }
