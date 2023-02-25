16ac6db63707a4b30eac75ed681f5c990394a15f
stepa <probka2992@gmail.com>
2023-02-25 19:55:06 +0300

    Add card "shield 2"

---
+++ Indie Varvar's/Assets/Object/Cards/Cards/Shield 2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield2 : Card, IPlayableCard
+{
+    public void OnPlay()
+    {
+        CardGameController.Instance.Player.AddShield(2);
+    }
+}
