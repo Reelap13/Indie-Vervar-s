78846e5a98995808c92f441cf5c7822346bd84ec
Reelap13 <116920397+Reelap13@users.noreply.github.com>
2023-02-25 18:08:20 +0300

    Deck
    Create deck class. Add several method: TakeCard, DiscardCard, ResetDeck, ShuffleDeck and empty LoadDeck.

--- Indie Varvar's/Assets/Object/Deck/DeckController.cs
+++ Indie Varvar's/Assets/Object/Deck/DeckController.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DeckController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Card> _playableDeck = new List<Card>();
+    private List<Card> _discardDeck = new List<Card>();
+
+    private void Awake()
     {
-        
+        LoadDeck();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Card TakeCard()
     {
-        
+        if (_playableDeck.Count == 0)
+        {
+            ShuffleDeck();
+        }
+
+        Card topCard = _playableDeck[0];
+        _playableDeck.RemoveAt(0);
+        return topCard;
+    }
+
+    public void DiscardCard(Card card)
+    {
+        _discardDeck.Add(card);
+    }
+
+    private void ResetDeck()
+    {
+        foreach (Card card in _discardDeck)
+        {
+            _playableDeck.Add(card);
+        }
+        _discardDeck.Clear();
+
+        ShuffleDeck();
+    }
+
+    private void ShuffleDeck()
+    {
+        for (int i = 0; i < _playableDeck.Count; ++i)
+        {
+            int rnd = UnityEngine.Random.Range(0, _playableDeck.Count - 1);
+            Card temp = _playableDeck[rnd];
+            _playableDeck[rnd] = _playableDeck[i];
+            _playableDeck[i] = temp;
+        }
+    }
+
+    private void LoadDeck()
+    {
+
     }
 }
